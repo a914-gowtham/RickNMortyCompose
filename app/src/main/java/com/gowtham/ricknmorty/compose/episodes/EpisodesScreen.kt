9b0062270d4d0d0b9c8f472ce01420452614bc6a
Gowtham <gowthaman@nfnlabs.com>
2021-07-05 23:28:35 +0530

    episodes listing worked

--- app/src/main/java/com/gowtham/ricknmorty/compose/episodes/EpisodesScreen.kt
+++ app/src/main/java/com/gowtham/ricknmorty/compose/episodes/EpisodesScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
